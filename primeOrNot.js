4a91624f4c9d4f55c54d1b48143cf5eafa0734f0
aswin chandran <aswin1998ply@gmail.com>
2023-08-09 23:19:31 +0530

    prime or not

---
+++ primeOrNot.js
@@ -0,0 +1,15 @@
+var a = [3, 4, 5, 6, 7, 8];
+
+function test_prime(num) {
+  if (num <= 1) {
+    console.log(num, "Not a Prime Number");
+  }
+  for (let i = 2; i <= Math.sqrt(num); i++) {
+    if (num % i === 0) {
+      console.log(num, "Not a Prime Number");
+    }
+  }
+  console.log(num, "Is Prime Number");
+}
+
+console.log(test_prime(a[2]));
